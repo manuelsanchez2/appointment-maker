8310dc14aec406703c4f9b44fa4a00bb569e7de7
manuelsanchez2 <manusansan22@gmail.com>
2020-08-25 08:48:29 +0200

    Add the onChange event to all the inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
-function Form(props) {
+const Form = () => {
+  // Crear State de Citas
+  const [appointment, setAppointment] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    time: "",
+    problem: "",
+  });
+
+  // Funcion que se actualiza cada vez que el usuario escribe en un input
+  // Buenas practicas es llamarlo handleChange - pero puede ser lo que quieras
+
+  const handleChange = () => {
+    console.log("hola");
+  };
+
   return (
     <Fragment>
       <h2>Soy formulario, encantado</h2>
@@ -12,6 +28,7 @@ function Form(props) {
           name="pet"
           className="u-full-width"
           placeholder="Enter pet name"
+          onChange={handleChange}
         />
         <label>Owner Name</label>
         <input
@@ -19,24 +36,35 @@ function Form(props) {
           name="owner"
           className="u-full-width"
           placeholder="Enter owner name"
+          onChange={handleChange}
         />
         <label>Date</label>
-        <input type="date" name="date" className="u-full-width" />
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={handleChange}
+        />
         <label>Time of call</label>
         <input
           type="time"
           name="time"
           className="u-full-width"
           placeholder="Enter owner name"
+          onChange={handleChange}
         />
         <label>Reason</label>
-        <textarea className="u-full-width" name="problems"></textarea>
+        <textarea
+          className="u-full-width"
+          name="problems"
+          onChange={handleChange}
+        ></textarea>
         <button type="submit" className="u-full-width button-primary">
           Add appointment
         </button>
       </form>
     </Fragment>
   );
-}
+};
 
 export default Form;
